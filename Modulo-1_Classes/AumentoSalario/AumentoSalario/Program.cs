3fd458861171a199183934783e750f2020438c60
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-09-07 15:54:25 -0300

    Add: Exercicios de fixação

---
+++ Modulo-1_Classes/AumentoSalario/AumentoSalario/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace AumentoSalario;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Funcionario funcionario = new Funcionario();
+
+        Console.Write("Nome: ");
+        funcionario.Nome = Console.ReadLine();
+        Console.Write("Salario bruto: ");
+        funcionario.SalarioBruto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        Console.Write("Imposto: ");
+        funcionario.Imposto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        Console.WriteLine($"Funcionario: {funcionario}");
+        Console.Write("Digite uma porcentagem para aumentar o salario: ");
+        double aumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        funcionario.AumentarSalario(aumento);
+        Console.WriteLine($"Dados atualizados: {funcionario}");
+    }
+}
---
+++ Modulo-1_Classes/BoletimEscolar/BoletimEscolar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace BoletimEscolar;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno = new Aluno();
+
+        Console.Write("Nome do aluno: ");
+        aluno.Nome = Console.ReadLine();
+        Console.WriteLine("Digite as três notas do aluno: ");
+        aluno.Nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        aluno.Nota2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        aluno.Nota3 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        if (aluno.NotaFinal() > 60.0)
+        {
+            Console.WriteLine("Nota final = " + aluno.NotaFinal().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("APROVADO");
+        }
+        else
+        {
+            Console.WriteLine($"Nota Final: {aluno.NotaFinal().ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine("REPROVADO");
+            Console.WriteLine($"FALTARAM {aluno.NotaRestante().ToString("F2",CultureInfo.InvariantCulture)} Pontos");
+        }
+    }
+}
