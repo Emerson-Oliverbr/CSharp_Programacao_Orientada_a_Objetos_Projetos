e1f49c18211c0d82cac3767b2df7d798610a3b1f
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-09-06 21:05:39 -0300

    Add: Exercicio exemplo Controle de estoque

---
+++ Modulo-1_Classes/Estoque/Estoque/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Estoque;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Produto produto = new Produto();
+        
+        Console.WriteLine("Entre com os dados do Produto: ");
+        Console.Write("Nome: ");
+        produto.Nome = Console.ReadLine();
+        Console.Write("Preco unitário: ");
+        produto.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        Console.Write("Quantidade no estoque: ");
+        produto.Quantidade = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Dados do produto: {produto}");
+        Console.Write("Digite o número de produtos a ser adicionados do estoque: ");
+        int qtde = int.Parse(Console.ReadLine());
+        produto.AdicionarProdutos(qtde);
+        Console.WriteLine($"Dados do Atualizados: {produto}");
+        Console.Write("Digite o número de produtos a serem removido do estoque: ");
+        qtde = int.Parse(Console.ReadLine());
+        produto.RemoverProdutos(qtde);
+        Console.WriteLine($"Dados do Atualizados: {produto}");
+    }
+}
