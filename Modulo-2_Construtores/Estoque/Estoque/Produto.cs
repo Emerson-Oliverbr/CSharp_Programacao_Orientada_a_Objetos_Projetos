8ddaa4dfd0ea8566d037f87924d8b1184fe07f08
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-09-08 22:02:31 -0300

    Palavra this

--- Modulo-2_Construtores/Estoque/Estoque/Produto.cs
+++ Modulo-2_Construtores/Estoque/Estoque/Produto.cs
@@ -7,17 +7,19 @@ public class Produto
     public double Preco;
     public int Quantidade;
 
-    public Produto(string nome, double preco, int quantidade)
+    public Produto()
     {
-        Nome = nome;
-        Preco = preco;
-        Quantidade = quantidade;
+        Quantidade = 10;
     }
 
-    public Produto(string nome, double preco)
+    public Produto(string nome, double preco) : this()
     {
         Nome = nome;
         Preco = preco;
+    }
+    public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+    {
+        Quantidade = quantidade;      
     }
 
     public double ValorTotalEmEstoque()
